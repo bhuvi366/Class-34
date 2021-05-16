65a14a930ee2669988265e86cd37c3c00b5b0392
bhuvi366 <76648389+bhuvi366@users.noreply.github.com>
2021-05-16 17:43:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -22,27 +22,27 @@ function setup(){
     ground = new Ground(600,height,1200,20);
     
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-   box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-   box5 = new Box(810,160,70,70);
-   box6 = new Box(810,160,70,70);
-   box7 = new Box(810,160,70,70);
-   box8 = new Box(810,160,70,70);
-   box9 = new Box(810,160,70,70);
-   box10 = new Box(810,160,70,70);
-   box11 = new Box(810,160,70,70);
-   box12= new Box(810,160,70,70);
-   box13 = new Box(810,160,70,70);
-   box14 = new Box(810,160,70,70);
-   box15 = new Box(810,160,70,70);
-   box16 = new Box(810,160,70,70);
-   box17 = new Box(810,160,70,70);
-   box18 = new Box(810,160,70,70);
-   box19 = new Box(810,160,70,70);
-   box20 = new Box(810,160,70,70);
-   box21 = new Box(810,160,70,70);
+    box1 = new Box(900,150,70,70);
+    box2 = new Box(900,150,70,70);
+   box3 = new Box(900,150,70,70);
+    box4 = new Box(900,150,70,70);
+   box5 = new Box(900,150,70,70);
+   box6 = new Box(900,150,70,70);
+   box7 = new Box(900,150,70,70);
+   box8 = new Box(800,150,70,70);
+   box9 = new Box(800,150,70,70);
+   box10 = new Box(800,150,70,70);
+   box11 = new Box(800,150,70,70);
+   box12= new Box(800,150,70,70);
+   box13 = new Box(800,150,70,70);
+   box14 = new Box(800,150,70,70);
+   box15 = new Box(700,150,70,70);
+   box16 = new Box(700,150,70,70);
+   box17 = new Box(700,150,70,70);
+   box18 = new Box(700,150,70,70);
+   box19 = new Box(700,150,70,70);
+   box20 = new Box(700,150,70,70);
+   box21 = new Box(700,150,70,70);
 
     
 
@@ -62,7 +62,23 @@ background (255);
     ground.display();
     box3.display();
     box4.display();
-box5.display();
+    box5.display();
+    box6.display();
+box7.display();
+box8.display();
+box9.display();
+box10.display();
+box11.display();
+box12.display();
+box13.display();
+box14.display();
+box15.display();
+box16.display();
+box17.display();
+box18.display();
+box19.display();
+box20.display();
+box21.display();
 ball.display();
 
     rope.display();
